a4b17d6883f42b3ffaff18f5d4b4153a141e593b
Gena <genaios@gmail.com>
2026-01-03 21:54:41 +0200

    add fix date time to be in english

--- hosts/nixos/configuration.nix
+++ hosts/nixos/configuration.nix
@@ -27,9 +27,16 @@
   time.timeZone = "Asia/Jerusalem";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_IL";
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.supportedLocales = [
+    "en_US.UTF-8/UTF-8"
+    "en_GB.UTF-8/UTF-8"
+    "he_IL.UTF-8/UTF-8"
+  ];
 
   i18n.extraLocaleSettings = {
+    LC_TIME = "en_GB.UTF-8";
     LC_ADDRESS = "he_IL.UTF-8";
     LC_IDENTIFICATION = "he_IL.UTF-8";
     LC_MEASUREMENT = "he_IL.UTF-8";
@@ -38,8 +45,7 @@
     LC_NUMERIC = "he_IL.UTF-8";
     LC_PAPER = "he_IL.UTF-8";
     LC_TELEPHONE = "he_IL.UTF-8";
-    LC_TIME = "he_IL.UTF-8";
-  };
+      };
 
   # Enable the X11 windowing system.
   services.xserver.enable = true;
