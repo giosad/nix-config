ab3e5141c8573b9dbfefede9c1a40fb0311dec45
Gennadi Iosad <genaios@gmail.com>
2026-01-17 13:37:26 +0200

    chore(pushkin): update disk and hardware config for bare metal

--- hosts/pushkin-nixos/disk-config.nix
+++ hosts/pushkin-nixos/disk-config.nix
@@ -3,7 +3,7 @@
     disk = {
       main = {
         type = "disk";
-        device = "/dev/sda";
+        device = "/dev/disk/by-id/ata-SanDisk_SSD_PLUS_240GB_1907DB801623";
         content = {
           type = "gpt";
           partitions = {
@@ -25,6 +25,40 @@
                 type = "filesystem";
                 format = "ext4";
                 mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+      ssd = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-SanDisk_SSD_PLUS_1000GB_214401A00277";
+        content = {
+          type = "gpt";
+          partitions = {
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/mnt/ssd";
+              };
+            };
+          };
+        };
+      };
+      hdd = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-WDC_WD10EZEX-00RKKA0_WD-WCC1S4603472";
+        content = {
+          type = "gpt";
+          partitions = {
+            root = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/mnt/hdd";
               };
             };
           };
--- hosts/pushkin-nixos/hardware-configuration.nix
+++ hosts/pushkin-nixos/hardware-configuration.nix
@@ -6,24 +6,11 @@
 {
   imports = [ ];
 
-  boot.initrd.availableKernelModules = [ "sd_mod" "sr_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  # fileSystems."/" =
-  #   { device = "/dev/disk/by-uuid/5d434bef-f5c7-4b54-a639-7ff3e2af275a";
-  #     fsType = "ext4";
-  #   };
-
-  # fileSystems."/boot" =
-  #   { device = "/dev/disk/by-uuid/2C9B-1A40";
-  #     fsType = "vfat";
-  #     options = [ "fmask=0077" "dmask=0077" ];
-  #   };
-
-  # swapDevices = [ ];
-
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  virtualisation.hypervGuest.enable = true;
+  # virtualisation.hypervGuest.enable = true;
 }
