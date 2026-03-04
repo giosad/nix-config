edfbc76304dd8eab60418f720f09a1890e500caa
Gennadi Iosad <gena@lightricks.com>
2026-03-04 16:25:24 +0200

    hm: add worktrunk integration

--- home/gena.nix
+++ home/gena.nix
@@ -121,7 +121,9 @@ in
                 dir=$(dirname "$dir")
             done
             echo "No virtual environment found."
-        }      
+        }
+
+        if command -v wt >/dev/null 2>&1; then eval "$(command wt config shell init zsh)"; fi
       ''
     ];
 
