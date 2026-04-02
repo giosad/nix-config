3254b33e14d23fea40f2a658dbf5c0b29dc53a98
Gennadi Iosad <genaios@gmail.com>
2026-04-02 20:38:01 +0300

    fix: update flake reference in update-nixos.sh to use hypr-desktop

--- update-nixos.sh
+++ update-nixos.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env zsh
 set -euo pipefail
-sudo nixos-rebuild switch --flake /home/gena/nix-config#nixos-hyperland
+sudo nixos-rebuild switch --flake /home/gena/nix-config#nixos-hypr-desktop
